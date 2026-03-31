2155e0df23c6553ba4947567f5505195ced9dc67
Lavish-Pundir <your-new-lavishpundir90@gmail.com>
2026-03-31 16:12:26 +0530

    context api project

---
+++ 08minicontext/src/Components/Profile.jsx
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react'
+import UserContext from '../Context/UserContext'
+
+
+function Profile() {
+
+    const { user } = useContext(UserContext)
+
+   if (!user) {
+    return <div>Please login to view your profile</div>
+   }
+   return (
+    <div>Welcome to my page {user.username}</div>
+   )
+}
+
+export default Profile
